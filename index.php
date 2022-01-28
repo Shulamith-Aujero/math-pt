916e288781c97ca66e7b0e24ec045c6187c16fe1
Shulamith-Aujero <98571193+Shulamith-Aujero@users.noreply.github.com>
2022-01-28 18:58:41 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<header><title> 3RD QUARTER PERFORMANCE TASK IN MATHEMATICS 9 </title></header>
+
+<body background="blink.gif" >
+<center>
+<br>
+<br>
+<br>
+<img src="3RD QUARTER.png" height= "700" width="1270">
+<center>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
